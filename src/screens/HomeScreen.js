19fc99a38110ec7bb7b57d35f6b2fb7f4d7a30fc
TheSoloGod <kienaof94@gmail.com>
2021-09-14 22:54:14 +0700

    update numpad & board resolved

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,43 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity, SafeAreaView, Dimensions, ScrollView, TextInput} from 'react-native';
+import {View, Text, TouchableOpacity, SafeAreaView, Dimensions, ScrollView, TextInput, FlatList} from 'react-native';
 import axios from 'axios';
 import * as API from '../config/Api';
 
 export default function HomeScreen() {
-    const [board, setBoard] = useState([[0,0,0,0,9,0,0,7,0],[0,0,0,0,0,0,5,8,9],[0,0,0,2,0,0,1,0,0],[2,0,0,4,0,0,7,9,8],[4,5,7,8,0,0,0,6,0],[0,0,0,7,1,3,0,0,5],[0,0,1,6,0,0,9,0,7],[0,6,0,0,0,0,8,2,0],[0,8,2,5,0,4,6,1,0]]);
+    const WIDTH = Dimensions.get('window').width;
+    const board_reset = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
+    const [board_origin, setBoardOrigin] = useState();
+    const [board, setBoard] = useState(board_reset);
+    const [board_draft, setBoardDraft] = useState(board_reset);
+    const [board_resolved, setBoardResolved] = useState([]);
+    const numbers_pad = [1,2,3,4,5,6,7,8,9];
 
     useEffect(() => {
         axios.get(API.GET_RANDOM_BOARD('easy'))
             .then(res => {
-                // setBoard(res.data.board);
+                let board_data = res.data;
+                setBoardOrigin(board_data);
+                setBoard(board_data.board);
+                setBoardDraft(board_data.board);
             })
     }, []);
 
-    const WIDTH = Dimensions.get('window').width;
+    useEffect(() => {
+        if (board_origin) {
+            let params_encode = encodeParams(board_origin);
+            axios.post(API.SOLVE_BOARD, params_encode, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+                .then(res => {
+                    setBoardResolved(res.data.board);
+                })
+        }
+    }, [board_origin]);
+
+    const encodeBoard = (board) => board.reduce((result, row, i) => result + `%5B${encodeURIComponent(row)}%5D${i === board.length -1 ? '' : '%2C'}`, '')
+
+    const encodeParams = (params) =>
+        Object.keys(params)
+            .map(key => key + '=' + `%5B${encodeBoard(params[key])}%5D`)
+            .join('&');
 
     const renderBoard = (board) => {
         return (
@@ -162,16 +186,48 @@ export default function HomeScreen() {
         );
     };
 
+    const renderNumbersPad = ({item}) => {
+        return (
+            <TouchableOpacity
+                style={{
+                    width: WIDTH * 0.9 / 9,
+                    height: WIDTH * 0.9 / 9,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#dedede',
+                    borderColor: 'black',
+                    borderWidth: 1
+                }}
+                onPress={() => {
+
+                }}
+            >
+                <Text>
+                    {item}
+                </Text>
+            </TouchableOpacity>
+        );
+    };
+
     return (
         <SafeAreaView>
-            <ScrollView>
                 <View style={{
                     alignItems: 'center',
                     marginVertical: 20
                 }}>
                     {renderBoard(board)}
                 </View>
-            </ScrollView>
+            <View style={{
+                width: WIDTH,
+                alignItems: 'center',
+            }}>
+                <FlatList
+                    data={numbers_pad}
+                    renderItem={renderNumbersPad}
+                    ItemSeparatorComponent={() => {return (<View style={{width: WIDTH * 0.01}}/>)}}
+                    horizontal={true}
+                />
+            </View>
         </SafeAreaView>
     );
 }

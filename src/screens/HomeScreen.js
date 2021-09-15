d82724c5ce6bab4ad547d80afbc0849cc99bcaca
DatHQ4 <hoangdat1999hp@gmail.com>
2021-09-15 15:33:33 +0700

    render update

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,177 +1,118 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity, SafeAreaView, Dimensions, ScrollView, TextInput} from 'react-native';
+import { View, Text, TouchableOpacity, SafeAreaView, Dimensions, ScrollView, TextInput, FlatList } from "react-native";
 import axios from 'axios';
 import * as API from '../config/Api';
 
 export default function HomeScreen() {
     const [board, setBoard] = useState([[0,0,0,0,9,0,0,7,0],[0,0,0,0,0,0,5,8,9],[0,0,0,2,0,0,1,0,0],[2,0,0,4,0,0,7,9,8],[4,5,7,8,0,0,0,6,0],[0,0,0,7,1,3,0,0,5],[0,0,1,6,0,0,9,0,7],[0,6,0,0,0,0,8,2,0],[0,8,2,5,0,4,6,1,0]]);
+    const [board_draft, setBoardDraft] = useState([])
+    const [selected, setSelected] = useState('')
+    // setBoardDraft(board)
+    const numColumns = 9
+    const numColumns2 = 1
 
     useEffect(() => {
         axios.get(API.GET_RANDOM_BOARD('easy'))
             .then(res => {
                 // setBoard(res.data.board);
+                // setBoardDraft(res.data.board)
+                setBoardDraft(board)
             })
     }, []);
+    const arrayClone = (arr) =>  {
+        var i, copy;
 
+        if( Array.isArray( arr ) ) {
+            copy = arr.slice( 0 );
+            for( i = 0; i < copy.length; i++ ) {
+                copy[ i ] = arrayClone( copy[ i ] );
+            }
+            return copy;
+        } else if( typeof arr === 'object' ) {
+            throw 'Cannot clone array containing an object!';
+        } else {
+            return arr;
+        }
+    }
     const WIDTH = Dimensions.get('window').width;
-
-    const renderBoard = (board) => {
-        return (
+    const renderItem = ({item, index}) => {
+        let group_index = index;
+        return(
             <View style={{
-                borderColor: 'black',
-                borderWidth: 2
-            }}>
-                <View style={{
-                    flexDirection: 'row',
-                }}>
-                    <View>
-                        {renderGroup(board[0], 0)}
-                    </View>
-                    <View>
-                        {renderGroup(board[1], 1)}
-                    </View>
-                    <View>
-                        {renderGroup(board[2], 2)}
-                    </View>
-                </View>
-                <View style={{
-                    flexDirection: 'row',
-                }}>
-                    <View>
-                        {renderGroup(board[3], 3)}
-                    </View>
-                    <View>
-                        {renderGroup(board[4], 4)}
-                    </View>
-                    <View>
-                        {renderGroup(board[5], 5)}
-                    </View>
-                </View>
-                <View style={{
-                    flexDirection: 'row',
-                }}>
-                    <View>
-                        {renderGroup(board[6], 6)}
-                    </View>
-                    <View>
-                        {renderGroup(board[7], 7)}
-                    </View>
-                    <View>
-                        {renderGroup(board[8], 8)}
-                    </View>
-                </View>
+                    borderColor: 'black',
+                    borderWidth: 1
+                        }}>
+                <FlatList data={item} renderItem={({item, index}) => rendercell(item, index, group_index)} horizontal={true}/>
             </View>
-        );
-    };
-
-    const renderGroup = (group, index_group) => {
-        return (
-            <View style={{
-                borderColor: 'black',
-                borderWidth: 1
-            }}>
-                <View style={{
-                    flexDirection: 'row',
-                }}>
-                    <View>
-                        {renderCell(group[0], index_group, 0)}
-                    </View>
-                    <View>
-                        {renderCell(group[1], index_group, 1)}
-                    </View>
-                    <View>
-                        {renderCell(group[2], index_group, 2)}
-                    </View>
-                </View>
-                <View style={{
-                    flexDirection: 'row',
-                }}>
-                    <View>
-                        {renderCell(group[3], index_group, 3)}
-                    </View>
-                    <View>
-                        {renderCell(group[4], index_group, 4)}
-                    </View>
-                    <View>
-                        {renderCell(group[5], index_group, 5)}
-                    </View>
-                </View>
-                <View style={{
-                    flexDirection: 'row',
-                }}>
-                    <View>
-                        {renderCell(group[6], index_group, 6)}
-                    </View>
-                    <View>
-                        {renderCell(group[7], index_group, 7)}
-                    </View>
-                    <View>
-                        {renderCell(group[8], index_group, 8)}
-                    </View>
-                </View>
-            </View>
-        );
-    };
-
-    const renderCell = (cell, index_group, index_cell) => {
-        return (
-            <View>
-                {
-                    cell == 0
+        )
+    }
+    const rendercell = (item, index, group) => {
+        return(
+          <View>
+              {
+                  item === board[group][index] && item !== 0
                     ?
-                        <View style={{
-                            width: WIDTH * 0.9 / 9,
-                            height: WIDTH * 0.9 / 9,
-                            borderColor: 'black',
-                            borderWidth: 1,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            backgroundColor: board[index_group][index_cell] == 0 ? 'white' : 'gray',
-                        }}>
-                            <TextInput
-                                style={{
-                                    padding: 0,
-                                }}
-                                defaultValue={board[index_group][index_cell].toString()}
-                                textAlign={'center'}
-                                maxLength={1}
-                                keyboardType={'number-pad'}
-                                onChangeText={text => {
-                                    let board_clone = [...board];
-                                    board_clone[index_group][index_cell] = text;
-                                    setBoard(board_clone);
-                                }}
-                            />
-                        </View>
-                        :
-                        <View style={{
-                            width: WIDTH * 0.9 / 9,
-                            height: WIDTH * 0.9 / 9,
-                            borderColor: 'black',
-                            borderWidth: 1,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            backgroundColor: 'cyan',
-                        }}>
-                            <Text>
-                                {cell}
-                            </Text>
-                        </View>
-                }
-            </View>
-        );
-    };
 
+                    <View style={{
+                        width: WIDTH * 0.9 / 9,
+                        height: WIDTH * 0.9 / 9,
+                        borderColor: 'black',
+                        borderWidth: 1,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: 'cyan',
+                    }}>
+                        <Text>
+                            {item + ' ' + board_draft[group][index]}
+
+                        </Text>
+                    </View>
+                    :
+                    <TouchableOpacity style={{
+                        width: WIDTH * 0.9 / 9,
+                        height: WIDTH * 0.9 / 9,
+                        borderColor: 'black',
+                        borderWidth: 1,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: `${group}-${index}` === selected ? 'red' : 'white',
+                    }}
+                    onPress={()=> {
+                        setSelected(`${group}-${index}`)
+                    }}>
+                    </TouchableOpacity>
+              }
+          </View>
+        )
+    }
     return (
         <SafeAreaView>
-            <ScrollView>
                 <View style={{
                     alignItems: 'center',
                     marginVertical: 20
                 }}>
-                    {renderBoard(board)}
+                    {/*{renderBoard(board)}*/}
+                    <FlatList data={board_draft} renderItem={renderItem} />
+                    <TouchableOpacity onPress={() => {
+                        let draft_clone = arrayClone(board_draft)
+                        let x = selected.split('-')[0];
+                        let y = selected.split('-')[1];
+                        draft_clone[x][y] = 8;
+                        console.log(board);
+                        console.log(board_draft);
+                        console.log(draft_clone);
+                        setBoardDraft(draft_clone);
+                        // setBoard(draft_clone)
+                        // console.log(board);
+                        // console.log(board_draft);
+                    }}>
+                        <Text>
+                            0
+                        </Text>
+                    </TouchableOpacity>
                 </View>
-            </ScrollView>
+
         </SafeAreaView>
     );
+
 }

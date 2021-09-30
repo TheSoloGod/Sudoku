2243a6c79877e5b52a2043ccb6ca6659bee25f47
DatHQ4 <hoangdat1999hp@gmail.com>
2021-09-30 15:58:49 +0700

    sudoku_superBasic

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import HomeScreen from './src/screens/HomeScreen';
+import GameScreen from './src/screens/GameScreen';
+import HomeScreen from "./src/screens/HomeScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 const App: () => Node = () => {
   return (
-      <HomeScreen/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <HomeScreen/>
+
+      // <GameScreen/>
   );
 };
 
